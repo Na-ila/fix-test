f68879ad06cc4d6d8da059ded178931ab4b14386
Хабибуллина Наиля Радиковна <nailya.khabibullina@tattelecom.ru>
2021-10-29 09:18:17 +0300

    added sounds

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from 'react'
 import {observer} from 'mobx-react-lite'
+import Sound from 'react-sound'
+
+import RnB from './assests/RnB.mp3'
+import Pop from './assests/Pop.mp3'
+import Electrohouse from './assests/Electrohouse.mp3'
 
 import store from './components/store/store'
 import Music from './components/Music'
@@ -46,28 +51,36 @@ const App = observer(() => {
   const classes = useStyles()
 
   return (
-    <div className={classes.root}>
-      <div className={classes.leftBlock}>
-        <div className={classes.musicBlock}>
-          <div className={classes.musicList}>
-            <Filter/>
-            <Music/>
+    <>
+      <div className={classes.root}>
+        <div className={classes.leftBlock}>
+          <div className={classes.musicBlock}>
+            <div className={classes.musicList}>
+              <Filter/>
+              <Music/>
+            </div>
+            <div
+            style={{width: '40%'}}
+            >
+              <CurrMusic/>
+            </div>
           </div>
-          <div
-          style={{width: '40%'}}
-          >
-            <CurrMusic/>
-          </div>
+          <Paper>
+            Animation
+          </Paper>
         </div>
-        <Paper>
-          Animation
-        </Paper>
+        <div className={classes.visitorList}>
+          <Filter visitorList/>
+          <Visitors/> 
+        </div>
       </div>
-      <div className={classes.visitorList}>
-        <Filter visitorList/>
-        <Visitors/> 
-      </div>
-    </div>
+      {store.selectedMusicToPlay &&
+      <Sound
+        url={store.selectedMusicToPlay.genre === 'RnB' ? RnB : store.selectedMusicToPlay.genre === 'Pop' ? Pop : Electrohouse}
+        playStatus={store.isPlaying ? Sound.status.PLAYING : Sound.status.PAUSED}
+      />
+      }
+    </>
   );
 })
 
