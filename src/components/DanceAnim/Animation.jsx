4f5647de656cf5c450009a41ed3d2b0f249c9d71
Хабибуллина Наиля Радиковна <nailya.khabibullina@tattelecom.ru>
2021-10-29 11:00:01 +0300

    waiting gif

--- src/components/DanceAnim/Animation.jsx
+++ src/components/DanceAnim/Animation.jsx
@@ -6,6 +6,7 @@ import store from '../store/store'
 import RnB_dance from '../../assests/RnB_dance.gif'
 import Pop_dance from '../../assests/Pop_dance.gif'
 import Electrohouse_dance from '../../assests/Electrohouse_dance.gif'
+import Waiting from '../../assests/Waiting.gif'
 
 import { makeStyles } from '@mui/styles';
 
@@ -20,10 +21,14 @@ const Animation = observer(() => {
 
     return (
         <div className={classes.root}>
-            {store.selectedMusicToPlay ? store.selectedMusicToPlay.genre === 'Pop' ?
-                <img src={Pop_dance} alt="Pop_dance" /> : store.selectedMusicToPlay.genre === 'RnB' ?
-                <img src={RnB_dance} alt="RnB_dance" /> : store.selectedMusicToPlay.genre === 'Electrohouse' ?
-                <img src={Electrohouse_dance} alt="Electrohouse_dance" /> : null
+            {store.selectedMusicToPlay ? 
+                store.isPlaying ?
+                store.selectedMusicToPlay.genre === 'Pop' ?
+                    <img src={Pop_dance} alt="Pop_dance" /> : store.selectedMusicToPlay.genre === 'RnB' ?
+                    <img src={RnB_dance} alt="RnB_dance" /> : store.selectedMusicToPlay.genre === 'Electrohouse' ?
+                    <img src={Electrohouse_dance} alt="Electrohouse_dance" /> 
+                : null
+                : <img src={Waiting} alt="Waiting" /> 
             : null}
         </div>
     );
